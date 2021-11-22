9177679693d5f6bc6cc9aeba7fa6c71e08c78fba
Thibaut Meslin <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2021-11-22 10:12:41 +0100

    Structure

---
+++ EasySave/EasySave/EasySave/Model/SaveWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Model
+{
+    class SaveWork
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string name;
+
+        publix string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
